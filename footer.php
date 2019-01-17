91a7f61de751acf8020fa8db40d8d2e427333924
AndrewSepic <andrew@thinkupdesign.ca>
2019-01-17 18:49:41 -0500

    Adding ACFpro and templating footer.php with fields and options pages

--- footer.php
+++ footer.php
@@ -1,37 +1,82 @@
 <?php
 /**
- * The template for displaying the footer. 
+ * The template for displaying the footer.
  *
  * Comtains closing divs for header.php.
  *
  * For more info: https://developer.wordpress.org/themes/basics/template-files/#template-partials
- */			
+ */
  ?>
-					
+
 				<footer class="footer" role="contentinfo">
-					
-					<div class="inner-footer grid-x grid-margin-x grid-padding-x">
-						
-						<div class="small-12 medium-12 large-12 cell">
-							<nav role="navigation">
-	    						<?php joints_footer_links(); ?>
-	    					</nav>
-	    				</div>
-						
-						<div class="small-12 medium-12 large-12 cell">
-							<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
-						</div>
-					
-					</div> <!-- end #inner-footer -->
-				
-				</footer> <!-- end .footer -->
-			
-			</div>  <!-- end .off-canvas-content -->
-					
-		</div> <!-- end .off-canvas-wrapper -->
-		
+
+          <div class="top-footer grid-x">
+
+  						<div class="small-12 medium-8 large-8 cell newsletter">
+                	<h4> Join Our Newsletter</h4>
+                  <input placeholder="Enter your email"></input>
+  	    			</div>
+
+              <div class="small-12 medium-4 large-4 cell latest-news">
+                <a href="http://www.facebook.com/pages/HowardCenter/106516727431" target="_blank"><span class="circle"><i class="fi-social-facebook"></i></span></a>
+                <a href="https://twitter.com/HowardCenterVT" target="_blank"><span class="circle"><i class="fi-social-twitter"></i></span></a>
+  	    			</div>
+
+  					</div> <!-- end #top-footer -->
+
+            <!-- Bottom Footer -->
+            <div class="bottom-footer grid-x grid-margin-x grid-padding-x">
+
+                <div class="small-12 medium-6 large-6 cell">
+                  <h3> <?php the_field('first_column_title', 'option'); ?></h3>
+                  <p>
+                    <?php the_field('vermont_afterschool_about', 'option');?>
+                  </p>
+                </div>
+
+                <div class="small-12 medium-6 large-3 cell">
+                  <h3><?php the_field('second_column_title', 'option'); ?></h3>
+                  <p>
+                    <?php the_field('contact_information', 'option'); ?>
+                  </p>
+                </div>
+
+                <div class="small-12 medium-6 large-3 cell">
+                  <h2><?php the_field('third_column_title', 'option'); ?></h2>
+                  <ul class="quickLinks">
+                    <?php
+                    // check if the repeater field has rows of data
+                      if( have_rows('quick_links', 'option') ):
+
+                        // loop through the rows of data
+                          while ( have_rows('quick_links', 'option') ) : the_row();   ?>
+
+                            <li> <a href="<?php the_sub_field('quick_link_link', 'option')?>"><?php the_sub_field('quick_link_text', 'option');?></a></li>   <?php
+
+                          endwhile;
+
+                      else :
+                          // no rows found
+                      endif;
+                      ?>
+                  </ul>
+
+                </div>
+
+              <div class="small-12 medium-12 large-12 cell copy">
+                <p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. | <a href="/privacy-policy">Privacy Policy</a></p>
+              </div>
+
+            </div><!-- end .bottom-footer -->
+
+  				</footer> <!-- end .footer -->
+
+  			</div>  <!-- end .off-canvas-content -->
+
+  		</div> <!-- end .off-canvas-wrapper -->
+
 		<?php wp_footer(); ?>
-		
+
 	</body>
-	
+
 </html> <!-- end page -->
--- functions.php
+++ functions.php
@@ -1,47 +1,79 @@
 <?php
-/** 
+/**
  * For more info: https://developer.wordpress.org/themes/basics/theme-functions/
  *
- */			
-	
+ */
+
 // Theme support options
-require_once(get_template_directory().'/functions/theme-support.php'); 
+require_once(get_template_directory().'/functions/theme-support.php');
 
 // WP Head and other cleanup functions
-require_once(get_template_directory().'/functions/cleanup.php'); 
+require_once(get_template_directory().'/functions/cleanup.php');
 
 // Register scripts and stylesheets
-require_once(get_template_directory().'/functions/enqueue-scripts.php'); 
+require_once(get_template_directory().'/functions/enqueue-scripts.php');
 
 // Register custom menus and menu walkers
-require_once(get_template_directory().'/functions/menu.php'); 
+require_once(get_template_directory().'/functions/menu.php');
 
 // Register sidebars/widget areas
-require_once(get_template_directory().'/functions/sidebar.php'); 
+require_once(get_template_directory().'/functions/sidebar.php');
 
 // Makes WordPress comments suck less
-require_once(get_template_directory().'/functions/comments.php'); 
+require_once(get_template_directory().'/functions/comments.php');
 
 // Replace 'older/newer' post links with numbered navigation
-require_once(get_template_directory().'/functions/page-navi.php'); 
+require_once(get_template_directory().'/functions/page-navi.php');
 
 // Adds support for multiple languages
-require_once(get_template_directory().'/functions/translation/translation.php'); 
+require_once(get_template_directory().'/functions/translation/translation.php');
 
 // Adds site styles to the WordPress editor
-// require_once(get_template_directory().'/functions/editor-styles.php'); 
+// require_once(get_template_directory().'/functions/editor-styles.php');
 
 // Remove Emoji Support
-// require_once(get_template_directory().'/functions/disable-emoji.php'); 
+// require_once(get_template_directory().'/functions/disable-emoji.php');
 
 // Related post function - no need to rely on plugins
-// require_once(get_template_directory().'/functions/related-posts.php'); 
+// require_once(get_template_directory().'/functions/related-posts.php');
 
 // Use this as a template for custom post types
 // require_once(get_template_directory().'/functions/custom-post-type.php');
 
 // Customize the WordPress login menu
-// require_once(get_template_directory().'/functions/login.php'); 
+// require_once(get_template_directory().'/functions/login.php');
 
 // Customize the WordPress admin
-// require_once(get_template_directory().'/functions/admin.php'); 
+// require_once(get_template_directory().'/functions/admin.php');
+
+// Adding Options Pages for ACF
+
+if( function_exists('acf_add_options_page') ) {
+
+	acf_add_options_page(array(
+		'page_title' 	=> 'Vermont After School Site Options',
+		'menu_title'	=> 'VTAS Site Options',
+		'menu_slug' 	=> 'vtas-site-options',
+		'capability'	=> 'edit_posts',
+		'parent_slug' => '',
+		'position'	=> false,
+		'icon_url'	=> false,
+	));
+
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'VTAS Homepage Settings',
+		'menu_title'	=> 'Homepage',
+		'parent_slug'	=> 'vtas-site-options',
+		'position'	=> false,
+		'icon_url'	=> false,
+	));
+
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'VTAS Footer Settings',
+		'menu_title'	=> 'Footer',
+		'parent_slug'	=> 'vtas-site-options',
+		'position'	=> false,
+		'icon_url'	=> false,
+	));
+
+}
