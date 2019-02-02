16594cee63149b453106396c8ec25ee44ebc102e
AndrewSepic <andrew@thinkupdesign.ca>
2019-02-01 19:13:25 -0500

    Homepage dev, markup and styles

--- footer.php
+++ footer.php
@@ -9,65 +9,67 @@
  ?>
 
 				<footer class="footer" role="contentinfo">
+        <!--  <div class="grid-container"> -->
 
-          <div class="top-footer grid-x">
+            <div class="top-footer grid-x">
 
-  						<div class="small-12 medium-8 large-8 cell newsletter">
-                	<h4> Join Our Newsletter</h4>
-                  <input placeholder="Enter your email"></input>
-  	    			</div>
+    						<div class="small-12 medium-8 large-8 cell newsletter">
+                  	<h4> Join Our Newsletter</h4>
+                    <input placeholder="Enter your email"></input>
+    	    			</div>
 
-              <div class="small-12 medium-4 large-4 cell latest-news">
-                <a href="http://www.facebook.com/pages/HowardCenter/106516727431" target="_blank"><span class="circle"><i class="fi-social-facebook"></i></span></a>
-                <a href="https://twitter.com/HowardCenterVT" target="_blank"><span class="circle"><i class="fi-social-twitter"></i></span></a>
-  	    			</div>
+                <div class="small-12 medium-4 large-4 cell latest-news">
+                  <a href="http://www.facebook.com/pages/HowardCenter/106516727431" target="_blank"><span class="circle"><i class="fi-social-facebook"></i></span></a>
+                  <a href="https://twitter.com/HowardCenterVT" target="_blank"><span class="circle"><i class="fi-social-twitter"></i></span></a>
+    	    			</div>
 
-  					</div> <!-- end #top-footer -->
+    					</div> <!-- end #top-footer -->
 
-            <!-- Bottom Footer -->
-            <div class="bottom-footer grid-x grid-margin-x grid-padding-x">
+              <!-- Bottom Footer -->
+              <div class="bottom-footer grid-x grid-margin-x grid-padding-x">
 
-                <div class="small-12 medium-6 large-6 cell">
-                  <h3> <?php the_field('first_column_title', 'option'); ?></h3>
-                  <p>
-                    <?php the_field('vermont_afterschool_about', 'option');?>
-                  </p>
+                  <div class="small-12 medium-6 large-6 cell">
+                    <h3> <?php the_field('first_column_title', 'option'); ?></h3>
+                    <p>
+                      <?php the_field('vermont_afterschool_about', 'option');?>
+                    </p>
+                  </div>
+
+                  <div class="small-12 medium-6 large-3 cell">
+                    <h3><?php the_field('second_column_title', 'option'); ?></h3>
+                    <p>
+                      <?php the_field('contact_information', 'option'); ?>
+                    </p>
+                  </div>
+
+                  <div class="small-12 medium-6 large-3 cell">
+                    <h2><?php the_field('third_column_title', 'option'); ?></h2>
+                    <ul class="quickLinks">
+                      <?php
+                      // check if the repeater field has rows of data
+                        if( have_rows('quick_links', 'option') ):
+
+                          // loop through the rows of data
+                            while ( have_rows('quick_links', 'option') ) : the_row();   ?>
+
+                              <li> <a href="<?php the_sub_field('quick_link_link', 'option')?>"><?php the_sub_field('quick_link_text', 'option');?></a></li>   <?php
+
+                            endwhile;
+
+                        else :
+                            // no rows found
+                        endif;
+                        ?>
+                    </ul>
+
+                  </div>
+
+                <div class="small-12 medium-12 large-12 cell copy">
+                  <p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. | <a href="/privacy-policy">Privacy Policy</a></p>
                 </div>
 
-                <div class="small-12 medium-6 large-3 cell">
-                  <h3><?php the_field('second_column_title', 'option'); ?></h3>
-                  <p>
-                    <?php the_field('contact_information', 'option'); ?>
-                  </p>
-                </div>
-
-                <div class="small-12 medium-6 large-3 cell">
-                  <h2><?php the_field('third_column_title', 'option'); ?></h2>
-                  <ul class="quickLinks">
-                    <?php
-                    // check if the repeater field has rows of data
-                      if( have_rows('quick_links', 'option') ):
-
-                        // loop through the rows of data
-                          while ( have_rows('quick_links', 'option') ) : the_row();   ?>
-
-                            <li> <a href="<?php the_sub_field('quick_link_link', 'option')?>"><?php the_sub_field('quick_link_text', 'option');?></a></li>   <?php
-
-                          endwhile;
-
-                      else :
-                          // no rows found
-                      endif;
-                      ?>
-                  </ul>
-
-                </div>
-
-              <div class="small-12 medium-12 large-12 cell copy">
-                <p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. | <a href="/privacy-policy">Privacy Policy</a></p>
-              </div>
-
-            </div><!-- end .bottom-footer -->
+              </div><!-- end .bottom-footer -->
+          <!--  </div>  end Grid Container -->
 
   				</footer> <!-- end .footer -->
 
--- homepage.php
+++ homepage.php
@@ -7,18 +7,67 @@ get_header(); ?>
 
 	<div class="content">
 
-		<div class="inner-content grid-x grid-margin-x grid-padding-x">
-				<div id="hero">
-					
-				</div>
+		<div class="inner-content grid-x grid-margin-x">
+			<?php $hero = get_field('hero_section', 'option');
+
+				if ( $hero ): ?>
+					<div id="hero" style="background-image: url('<?php echo $hero['image']['url'];?>') ">
+						<img src="<?php echo $hero['image']['url'];?>"/>
+						<div id="heroTitle">
+							<span class="intro"><?php echo $hero['intro']; ?></span>
+							<h4><?php echo $hero['headline']; ?></h4>
+							<span class="outro"><?php echo $hero['outro']; ?></span>
+						</div>
+					</div>
+				<?php endif; ?>
 
 		    <main class="main small-12 medium-12 large-12 cell" role="main">
 
-				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					<!-- First Section -->
+						<div class="cell small-12 medium-12 large-12 home-1">
+							<h2>We Are Vermont Afterschool</h2>
+							<p>A staewide non-profit The grid defaults to the full width of the available space. To contain it use the grid-container class. The container will be centered and have a max-width equal to your $grid-container setting (1200px by default), along with padding on the left/right equal to half your $grid-container-padding setting.</p>
+						</div>
 
-					<?php get_template_part( 'parts/loop', 'page' ); ?>
+					<!-- Second Section -->
+						<div class="cell small-12 medium-12 large-12 home-2">
+							<div class="cell small-12 medium-6 large-6 large-offset-6 medium-offset-6">
+								<h2>Why We Do This Work</h2>
+								<p>A staewide non-profit The grid defaults to the full width of the available space. To contain it use the grid-container class. </p>
+								<a class="button">Impacts</a>
+							</div>
+						</div>
 
-				<?php endwhile; endif; ?>
+					<!-- Third Section -->
+					<div class="cell small-12 medium-12 large-12 home-3">
+						<div class="cell small-12 medium-6 large-6">
+							<h2>We Rely on Colloboration</h2>
+							<p>A staewide non-profit The grid defaults to the full width of the available space. To contain it use the grid-container class. </p>
+							<a class="button orange">Join Us</a> <a class="button">Learn More</a>
+						</div>
+						<div class="cell small-12 medium-6 large-6 home-3">
+							Logos go hero
+						</div>
+					</div>
+
+					<div class="grid-x grid-padding-x need-to-know">
+						<div class="cell small-12 medium-12 large-12 header">
+							<h5>What you Need to Know</h5>
+							<a class="pagination">View All Posts &raquo;</a>
+						</div>
+
+
+							<div class="cell small-12 medium-4 large-4 post">
+								<span class="cat">Opinion</span>
+								<h2>Post Title With Two Lines</h2>
+								<span class="meta">By Author, Position</span>
+								<p class="excerpt">It was recently public radio fundraising time here in Vermont. Im a big VErmont Public Radio fan and have been knonw to stay tuned through their repeated membership pitches.</p>
+								<?php the_content('<button class="tiny">' . __( 'Read more...', 'jointswp' ) . '</button>'); ?>
+							</div>
+
+
+					</div>
+
 
 			</main> <!-- end #main -->
 
