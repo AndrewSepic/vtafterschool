2e78aa04bf74ac65448985eb9522ddb93f4a5a15
AndrewSepic <andrew@thinkupdesign.ca>
2019-06-05 18:07:28 -0400

    styles and templates custom pageheaders

--- page-with-intro.php
+++ page-with-intro.php
@@ -1,18 +1,17 @@
 <?php
-/**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- */
+/*
+Template Name: Page with Header Intro
+*/
 
 get_header(); ?>
 
-	<div class="content">
+	<div class="content pageIntro">
 		<div class="page-header">
 			<div class="grid-container">
 				<div class="grid-x grid-margin-x grid-padding-x">
 					<header class="article-header">
 						<h1 class="page-title"><?php the_title(); ?></h1>
+						<div class="introText"><?php the_field('intro_text');?></div>
 					</header> <!-- end article header -->
 				</div>
 			</div>
@@ -20,20 +19,20 @@ get_header(); ?>
 
 		<div class="grid-container">
 			<div class="inner-content grid-x grid-margin-x grid-padding-x">
-			    <main class="main small-12 large-8 medium-8 cell" role="main">
+	    	<main class="main small-12 large-8 medium-8 cell" role="main">
 
-					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-				    	<?php get_template_part( 'parts/loop', 'page' ); ?>
+		    	<?php get_template_part( 'parts/loop', 'page' ); ?>
 
-				    <?php endwhile; endif; ?>
+		    <?php endwhile; endif; ?>
 
 				</main> <!-- end #main -->
 
-			    <?php get_sidebar(); ?>
+	    <?php get_sidebar(); ?>
 
 			</div> <!-- end #inner-content -->
-		</div>
+
 	</div> <!-- end #content -->
 
 <?php get_footer(); ?>
