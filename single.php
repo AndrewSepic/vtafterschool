5abdb188714d0c5eb60ce05516d295cb44defd61
AndrewSepic <andrew@thinkupdesign.ca>
2019-07-17 13:47:17 -0400

    stuff added in styles for single posts, byline, forms styles

--- single.php
+++ single.php
@@ -1,32 +1,44 @@
-<?php 
+<?php
 /**
  * The template for displaying all single posts and attachments
  */
 
 get_header(); ?>
-			
+
 <div class="content">
 
-	<div class="inner-content grid-x grid-margin-x grid-padding-x">
+	<div class="page-header">
+		<div class="grid-container">
+			<div class="grid-x grid-margin-x grid-padding-x">
+				<header class="article-header">
+					<h1 class="page-title">Blog</h1>
+				</header> <!-- end article header -->
+			</div>
+		</div>
+	</div>
 
-		<main class="main small-12 medium-8 large-8 cell" role="main">
-		
-		    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		
-		    	<?php get_template_part( 'parts/loop', 'single' ); ?>
-		    	
-		    <?php endwhile; else : ?>
-		
-		   		<?php get_template_part( 'parts/content', 'missing' ); ?>
+	<div class="grid-container">
 
-		    <?php endif; ?>
+		<div class="inner-content grid-x grid-margin-x grid-padding-x">
 
-		</main> <!-- end #main -->
+			<main class="main small-12 medium-8 large-8 cell" role="main">
 
-		<?php get_sidebar(); ?>
+			    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-	</div> <!-- end #inner-content -->
+			    	<?php get_template_part( 'parts/loop', 'single' ); ?>
 
+			    <?php endwhile; else : ?>
+
+			   		<?php get_template_part( 'parts/content', 'missing' ); ?>
+
+			    <?php endif; ?>
+
+			</main> <!-- end #main -->
+
+			<?php get_sidebar(); ?>
+
+		</div> <!-- end #inner-content -->
+	</div> <!-- end .grid-container -->
 </div> <!-- end #content -->
 
 <?php get_footer(); ?>
